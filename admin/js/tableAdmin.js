5129c03f8ee8ce62e979c8c79c6d2b0566f03369
WeeenTB <owentb125@gmail.com>
2023-12-06 20:40:21 +0700

    Modal (Pop up) untuk tombol i

--- admin/js/tableAdmin.js
+++ admin/js/tableAdmin.js
@@ -5,92 +5,120 @@ var message;
 const { jwtToken, jwtData } = checkAuth("Admin", window.location.href);
 
 function populateTable() {
-	var tableBody = document.querySelector("#myTable tbody");
+    var tableBody = document.querySelector("#myTable tbody");
 
-	// Ganti URL_API dengan URL sesuai dengan API yang ingin Anda gunakan
-	fetch(endpoint)
-		.then((response) => response.json())
-		.then((data) => {
-			// Periksa apakah data dari API tidak kosong
-			if (data && data.length > 0) {
-				data.forEach(function (shipment) {
-					var row = tableBody.insertRow();
-					row.innerHTML = `<th scope="row">${
-						shipment.tracking_number
-					}</th>
+    // Ganti URL_API dengan URL sesuai dengan API yang ingin Anda gunakan
+    fetch(endpoint)
+        .then((response) => response.json())
+        .then((data) => {
+            // Periksa apakah data dari API tidak kosong
+            if (data && data.length > 0) {
+                data.forEach(function (shipment) {
+                    var row = tableBody.insertRow();
+                    row.innerHTML = `
+                            <th scope="row">${shipment.tracking_number}</th>
                             <td>${shipment.sender_name}</td>
                             <td>${shipment.intended_receiver_name}</td>
                             <td>${shipment.receiver_address}</td>
                             <td class="onprocess popover-trigger">${getStatusBadge(
-								shipment
-							)}</td>`;
-				});
-				initializePopovers();
-			} else {
-				console.error("Error: Data from API is undefined or empty.");
-			}
-		})
-		.catch((error) => {
-			console.error("Error fetching data from API:", error);
-		});
+                        shipment
+                    )}</td>`;
+                });
+                initializePopovers();
+            } else {
+                console.error("Error: Data from API is undefined or empty.");
+            }
+        })
+        .catch((error) => {
+            console.error("Error fetching data from API:", error);
+        });
 }
 
 // Function to generate status badge based on status
 function getStatusBadge(shipment) {
-	var badgeClass = "";
-	var badgeContent = "";
+    var badgeClass = "";
+    var badgeContent = "";
 
-	switch (shipment.delivery_status) {
-		case "package_delivered":
-			badgeClass = "deliver badge badge-success";
-			badgeContent = "Delivered";
-			break;
-		case "delivery_failed":
-			badgeClass = "cancel";
-			badgeContent = `<button type="button" class="btn btn-secondary popover-trigger" data-bs-toggle="popover" data-bs-placement="top" title="Alasan Cancel" data-bs-content="${shipment.fail_message}">Cancel</button>`;
-			break;
-		case "returned_to_pool":
-			badgeClass = "cancel";
-			badgeContent = `<button type="button" class="btn btn-secondary popover-trigger" data-bs-toggle="popover" data-bs-placement="top" title="Alasan Cancel" data-bs-content="${shipment.fail_message}">Cancel</button>`;
-			break;
-		case "on_sender_pool":
-			badgeClass = "onprocess badge badge-warning";
-			badgeContent = `On Process`;
-			break;
-		case "dispatched":
-			badgeClass = "onprocess badge badge-warning";
-			badgeContent = "On Process";
-			break;
-		case "on_destination_pool":
-			badgeClass = "onprocess badge badge-warning";
-			badgeContent = "On Process";
-			break;
-		case "otw_receiver_address":
-			badgeClass = "onprocess badge badge-warning";
-			badgeContent = "On Process";
-			break;
-		default:
-			badgeClass = "badge badge-secondary";
-			badgeContent = "Unknown";
-	}
+    switch (shipment.delivery_status) {
+        case "package_delivered":
+            badgeClass = "deliver badge badge-success";
+            badgeContent = "Delivered";
+            break;
+        case "delivery_failed":
+            badgeClass = "cancel";
+            badgeContent = `<button type="button" class="btn btn-secondary popover-trigger" data-bs-toggle="popover" data-bs-placement="top" title="Alasan Cancel" data-bs-content="${shipment.fail_message}">Cancel</button>`;
+            break;
+        case "returned_to_pool":
+            badgeClass = "cancel";
+            badgeContent = `<button type="button" class="btn btn-secondary popover-trigger" data-bs-toggle="popover" data-bs-placement="top" title="Alasan Cancel" data-bs-content="${shipment.fail_message}">Cancel</button>`;
+            break;
+        case "on_sender_pool":
+            badgeClass = "onprocess badge badge-warning";
+            badgeContent = `On Process`;
+            break;
+        case "dispatched":
+            badgeClass = "onprocess badge badge-warning";
+            badgeContent = "On Process";
+            break;
+        case "on_destination_pool":
+            badgeClass = "onprocess badge badge-warning";
+            badgeContent = "On Process";
+            break;
+        case "otw_receiver_address":
+            badgeClass = "onprocess badge badge-warning";
+            badgeContent = "On Process";
+            break;
+        default:
+            badgeClass = "badge badge-secondary";
+            badgeContent = "Unknown";
+    }
 
-	return `<span class="${badgeClass}">${badgeContent}</span> <a href="/index.html" class="i"><span class="info badge badge-info">i</span><a>`;
+    return `<span class="${badgeClass}">${badgeContent}</span> <button type="button" class="btn btn-info infoBtn" data-bs-toggle="modal" data-bs-target="#infoModal" data-shipment='${JSON.stringify(shipment)}'>i</button>`;
 }
 
 function initializePopovers() {
-	var popoverTriggerList = [].slice.call(
-		document.querySelectorAll(".popover-trigger")
-	);
-	var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-		return new bootstrap.Popover(popoverTriggerEl);
-	});
+    var popoverTriggerList = [].slice.call(
+        document.querySelectorAll(".popover-trigger")
+    );
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl);
+    });
 }
 
-function logout() {
-	localStorage.removeItem("jwtToken");
-	window.location.assign("../../index.html");
+
+
+function displayInfoModal(shipmentData) {
+  var modalContent = `
+      <p><strong>Nomor Resi:</strong> ${shipmentData.tracking_number}</p>
+      <p><strong>Nama Pengirim:</strong> ${shipmentData.sender_name}</p>
+      <p><strong>Nama Penerima:</strong> ${shipmentData.intended_receiver_name}</p>
+      <p><strong>Alamat Penerima:</strong> ${shipmentData.receiver_address}</p>
+      <p><strong>Status:</strong> ${getStatusBadge(shipmentData)}</p>
+      <!-- Tambahkan informasi lain sesuai kebutuhan -->`;
+
+  var infoContent = document.querySelector('#infoModal .modal-body');
+  infoContent.innerHTML = modalContent;
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  var infoButtons = document.querySelectorAll('.infoBtn');
+
+  infoButtons.forEach(function (btn) {
+      btn.addEventListener('click', function () {
+          var shipmentData = JSON.parse(btn.getAttribute('data-shipment'));
+          displayInfoModal(shipmentData);
+      });
+  });
+});
 
 window.onload = function () {
-	populateTable();
+  populateTable();
 };
+
+
+function logout() {
+    localStorage.removeItem("jwtToken");
+    window.location.assign("../../index.html");
+}
+
+
