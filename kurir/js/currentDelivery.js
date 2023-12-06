23317735c52d597f8441a89a7524aa33f193a074
Nicky Franshel <nickyfranshel@gmail.com>
2023-12-06 11:25:25 +0700

    created protected routes based on roles

--- kurir/js/currentDelivery.js
+++ kurir/js/currentDelivery.js
@@ -1,12 +1,14 @@
-function toggleStatus(status) {
-  document
-    .querySelectorAll(".on-process, .delivered, .cancelled")
-    .forEach((div) => (div.style.display = "none"));
+const { jwtToken, jwtData } = checkAuth("Courier", window.location.href);
 
-  document.querySelector("." + status).style.display = "block";
+function toggleStatus(status) {
+	document
+		.querySelectorAll(".on-process, .delivered, .cancelled")
+		.forEach((div) => (div.style.display = "none"));
+
+	document.querySelector("." + status).style.display = "block";
 }
 
 function logout() {
-  localStorage.removeItem("jwtToken");
-  window.location.assign("../../index.html");
+	localStorage.removeItem("jwtToken");
+	window.location.assign("../../index.html");
 }
--- kurir/js/history.js
+++ kurir/js/history.js
@@ -1,81 +1,84 @@
 const endpoint = "https://localhost:7023/delivery";
 var shipmentData;
+const { jwtToken, jwtData } = checkAuth("Courier", window.location.href);
 
 function populateTable() {
-  var tableBody = document.querySelector("#myTable tbody");
+	var tableBody = document.querySelector("#myTable tbody");
 
-  // Ganti URL_API dengan URL sesuai dengan API yang ingin Anda gunakan
-  fetch(endpoint)
-    .then((response) => response.json())
-    .then((data) => {
-      // Periksa apakah data dari API tidak kosong
-      if (data && data.length > 0) {
-        data.forEach(function (shipment) {
-          var row = tableBody.insertRow();
-          row.innerHTML = `<th scope="row">${shipment.tracking_number}</th>
+	// Ganti URL_API dengan URL sesuai dengan API yang ingin Anda gunakan
+	fetch(endpoint)
+		.then((response) => response.json())
+		.then((data) => {
+			// Periksa apakah data dari API tidak kosong
+			if (data && data.length > 0) {
+				data.forEach(function (shipment) {
+					var row = tableBody.insertRow();
+					row.innerHTML = `<th scope="row">${
+						shipment.tracking_number
+					}</th>
                             <td>${shipment.actual_receiver_name}</td>
-                            <td>${shipment.reveiver_phone}</td>
+                            <td>${shipment.receiver_phone}</td>
                             <td>${shipment.receiver_address}</td>
                             <td>${getStatusBadge(
-                              shipment.delivery_status
-                            )}</td>`;
-        });
-      } else {
-        console.error("Error: Data from API is undefined or empty.");
-      }
-    })
-    .catch((error) => {
-      console.error("Error fetching data from API:", error);
-    });
+								shipment.delivery_status
+							)}</td>`;
+				});
+			} else {
+				console.error("Error: Data from API is undefined or empty.");
+			}
+		})
+		.catch((error) => {
+			console.error("Error fetching data from API:", error);
+		});
 }
 
 // Function to generate status badge based on status
 function getStatusBadge(delivery_status) {
-  var badgeClass = "";
-  var badgeContent = "";
+	var badgeClass = "";
+	var badgeContent = "";
 
-  switch (delivery_status) {
-    case "package_delivered":
-      badgeClass = "deliver badge badge-success";
-      badgeContent = "Delivered";
-      break;
-    case "delivery_failed":
-      badgeClass = "cancel badge badge-danger";
-      badgeContent = '<a href="/index.html">Canceled</a>';
-      break;
-    case "returned_to_pool":
-      badgeClass = "cancel badge badge-danger";
-      badgeContent = '<a href="/index.html">Canceled</a>';
-      break;
-    case "on_sender_pool":
-      badgeClass = "onprocess badge badge-warning";
-      badgeContent = "On Process";
-      break;
-    case "dispatched":
-      badgeClass = "onprocess badge badge-warning";
-      badgeContent = "On Process";
-      break;
-    case "on_destination_pool":
-      badgeClass = "onprocess badge badge-warning";
-      badgeContent = "On Process";
-      break;
-    case "otw_receiver_address":
-      badgeClass = "onprocess badge badge-warning";
-      badgeContent = "On Process";
-      break;
-    default:
-      badgeClass = "badge badge-secondary";
-      badgeContent = "Unknown";
-  }
+	switch (delivery_status) {
+		case "package_delivered":
+			badgeClass = "deliver badge badge-success";
+			badgeContent = "Delivered";
+			break;
+		case "delivery_failed":
+			badgeClass = "cancel badge badge-danger";
+			badgeContent = '<a href="/index.html">Canceled</a>';
+			break;
+		case "returned_to_pool":
+			badgeClass = "cancel badge badge-danger";
+			badgeContent = '<a href="/index.html">Canceled</a>';
+			break;
+		case "on_sender_pool":
+			badgeClass = "onprocess badge badge-warning";
+			badgeContent = "On Process";
+			break;
+		case "dispatched":
+			badgeClass = "onprocess badge badge-warning";
+			badgeContent = "On Process";
+			break;
+		case "on_destination_pool":
+			badgeClass = "onprocess badge badge-warning";
+			badgeContent = "On Process";
+			break;
+		case "otw_receiver_address":
+			badgeClass = "onprocess badge badge-warning";
+			badgeContent = "On Process";
+			break;
+		default:
+			badgeClass = "badge badge-secondary";
+			badgeContent = "Unknown";
+	}
 
-  return `<span class="${badgeClass}">${badgeContent}</span> <a href="/index.html" class="i"><span class="info badge badge-info">i</span><a>`;
+	return `<span class="${badgeClass}">${badgeContent}</span> <a href="/index.html" class="i"><span class="info badge badge-info">i</span><a>`;
 }
 
 function logout() {
-  localStorage.removeItem("jwtToken");
-  window.location.assign("../../index.html");
+	localStorage.removeItem("jwtToken");
+	window.location.assign("../../index.html");
 }
 
 window.onload = function () {
-  populateTable();
+	populateTable();
 };
